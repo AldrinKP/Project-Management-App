16e6ba169a47b04d2d79dc4989aa623db1b799be
AldrinKP <aldrinpastores@gmail.com>
2024-05-16 18:01:53 +0200

    Added styling to CreateProject component

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,16 +1,20 @@
 import { forwardRef } from 'react';
+const classes =
+	'w-full p-1 border-b-2 rounded-sm border-stone-300 bg-stone-200 text-stone-600 focus:outline-none focus:border-stone-600';
 
 const Input = forwardRef(function Input(
 	{ textarea, labelText, ...props },
 	ref
 ) {
 	return (
-		<p>
-			<label>{labelText}</label>
+		<p className="flex flex-col gap-1 my-4">
+			<label className="text-sm font-bold uppercase text-stone-500">
+				{labelText}
+			</label>
 			{textarea ? (
-				<textarea {...props} ref={ref} />
+				<textarea {...props} ref={ref} className={classes} />
 			) : (
-				<input {...props} ref={ref} />
+				<input {...props} ref={ref} className={classes} />
 			)}
 		</p>
 	);
