62015fdd1a469602a753ae4a934aa974d87c935b
AldrinKP <aldrinpastores@gmail.com>
2024-05-16 13:23:21 +0200

    Added styling to sidebar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,13 +1,16 @@
+import ProjectEntry from './ProjectEntry';
+
 export default function Sidebar() {
 	return (
 		<aside className="w-1/3 px-8 py-16 bg-stone-900 text-stone-50 md:w-72 rounded-r-xl">
+			<h2 className="mb-8 font-bold uppercase md:text-xl text-stone-200">
+				Your Projects
+			</h2>
 			<div className="flex items-center gap-4">
-				<h1>YOUR PROJECTS</h1>
-				<menu className="flex items-center justify-end gap-4 my-4">
-					<button className="px-4 py-2 text-xs md:text-base rounded-md bg-stone-700 text-stone-400 hover:bg-stone-600 hover:text-stone-100">
-						+ Add Project
-					</button>
-				</menu>
+				<button className="px-4 py-2 text-xs md:text-base rounded-md bg-stone-700 text-stone-400 hover:bg-stone-600 hover:text-stone-100">
+					+ Add Project
+				</button>
+				<ul className="mt-8"></ul>
 			</div>
 		</aside>
 	);
