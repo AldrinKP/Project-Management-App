6e0720d8c5448a7ece916ae74dee3e864a4b3a50
AldrinKP <aldrinpastores@gmail.com>
2024-05-16 12:24:00 +0200

    Created initial sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
+import Sidebar from './components/Sidebar';
+
 function App() {
-  return (
-    <>
-      <h1 className="my-8 text-center text-5xl font-bold">Hello World</h1>
-    </>
-  );
+	return (
+		<main className="h-screen my-8 flex gap-8">
+			<Sidebar />
+			<h1 className="my-8 text-center text-5xl font-bold">Hello World</h1>
+		</main>
+	);
 }
 
 export default App;
