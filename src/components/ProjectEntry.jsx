0cfbea680252a4c75bf8aea455d219a2e4a28ca8
AldrinKP <aldrinpastores@gmail.com>
2024-05-16 17:25:49 +0200

    Deleted ProjectEntry component

--- src/components/ProjectEntry.jsx
+++
@@ -1,7 +0,0 @@
-export default function ProjectEntry({ projectName }) {
-	return (
-		<button className="w-full text-left px-2 py-1 rounded-sm my-1 hover:text-stone-200 hover:bg-stone-800">
-			Project 1
-		</button>
-	);
-}
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,5 +1,3 @@
-import ProjectEntry from './ProjectEntry';
-
 export default function Sidebar({ createNewProject, projects, selectProject }) {
 	return (
 		<aside className="w-1/3 px-8 py-16 bg-stone-900 text-stone-50 md:w-72 rounded-r-xl">
