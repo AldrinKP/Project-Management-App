54eae3cacf8e42d224ff8fb14cba034acbf04fa4
AldrinKP <aldrinpastores@gmail.com>
2024-05-16 18:07:12 +0200

    Added styling

--- src/components/NoProject.jsx
+++ src/components/NoProject.jsx
@@ -2,14 +2,19 @@ import noProjects from '../assets/no-projects.png';
 
 export default function NoProject({ createNewProject }) {
 	return (
-		<div>
+		<div className="mt-24 text-center w-2/3">
 			<img
 				src={noProjects}
+				alt="An empty task list"
 				className="w-16 h-16 object-contain mx-auto"
 			/>
-			<h2>No Project Selected</h2>
-			<p>Select a project or get started with a new one</p>
-			<p>
+			<h2 className="text-xl font-bold text-stone-500 my-4">
+				No Project Selected
+			</h2>
+			<p className="text-stone-400 mb-4">
+				Select a project or get started with a new one
+			</p>
+			<p className="mt-8">
 				<button onClick={createNewProject}>Create new project</button>
 			</p>
 		</div>
