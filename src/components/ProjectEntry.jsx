c00a4bf45c8c142f85ee72b07e6c48788d14bd53
AldrinKP <aldrinpastores@gmail.com>
2024-05-16 13:23:36 +0200

    Created initial ProjectEntry component

---
+++ src/components/ProjectEntry.jsx
@@ -0,0 +1,7 @@
+export default function ProjectEntry({ projectName }) {
+	return (
+		<button className="w-full text-left px-2 py-1 rounded-sm my-1 hover:text-stone-200 hover:bg-stone-800">
+			Project 1
+		</button>
+	);
+}
