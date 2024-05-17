c4d60500e128434b097b7994ff46b4e318da16bb
AldrinKP <aldrinpastores@gmail.com>
2024-05-17 15:12:22 +0200

    Added styling

--- src/components/ProjectDetails.jsx
+++ src/components/ProjectDetails.jsx
@@ -51,13 +51,24 @@ export default function ProjectDetails({
 					{projectDetails.description}
 				</p>
 			</header>
-			<p>Tasks</p>
-			<div>
-				<input ref={task} />
-				<button onClick={handleAddTask}>Add Task</button>
+			<h2 className="text-2xl font-bold text-stone-700 mb-4">Tasks</h2>
+			<div className="flex items-center gap-4">
+				<input
+					ref={task}
+					type="text"
+					className="w-64 px-2 py-1 rounded-sm bg-stone-200"
+				/>
+				<button
+					onClick={handleAddTask}
+					className="text-stone-700 hover:bg-stone-950 hover:text-stone-200 py-1 px-2 rounded-md"
+				>
+					Add Task
+				</button>
 			</div>
 			{projectDetails.tasks.length === 0 ? (
-				<p>This project does not have any tasks yet</p>
+				<p className="text-stone-800 my-4">
+					This project does not have any tasks yet
+				</p>
 			) : (
 				<ul>
 					{projectDetails.tasks.map((task, index) => {
@@ -67,7 +78,10 @@ export default function ProjectDetails({
 								key={index}
 							>
 								<p>{task}</p>
-								<button onClick={() => handleRemoveTask(index)}>
+								<button
+									onClick={() => handleRemoveTask(index)}
+									className="text-stone-700 hover:bg-stone-950 hover:text-stone-200 py-1 px-2 rounded-md"
+								>
 									Clear
 								</button>
 							</li>
