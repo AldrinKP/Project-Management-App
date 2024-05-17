738d28127f51e2c3a8f26ae9e1dffb692dda902f
AldrinKP <aldrinpastores@gmail.com>
2024-05-17 14:37:39 +0200

    Added styling

--- src/components/ErrorModal.jsx
+++ src/components/ErrorModal.jsx
@@ -2,8 +2,14 @@ import { forwardRef } from 'react';
 
 const ErrorModal = forwardRef(function ErrorModal({}, ref) {
 	return (
-		<dialog ref={ref}>
-			<p>Please enter valid values</p>
+		<dialog
+			ref={ref}
+			className="backdrop:bg-stone-900/90 p-4 rounded-md shadow-md"
+		>
+			<h2 className="text-xl font-bold text-stone-500 my-4">
+				Invalid Input
+			</h2>
+			<p className="text-stone-400 mb-4">Please enter valid values</p>
 			<form method="dialog">
 				<button>Close</button>
 			</form>
