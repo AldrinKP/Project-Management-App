fa48a77b8d2d21af2bb1a443cabcb7eaef484dfb
AldrinKP <aldrinpastores@gmail.com>
2024-05-22 12:56:04 +0200

    Refactored app to use Projects context

--- src/App.jsx
+++ src/App.jsx
@@ -1,86 +1,15 @@
-import { useState } from 'react';
 import Sidebar from './components/Sidebar';
-import NoProject from './components/NoProject';
-import CreateProject from './components/CreateProject';
-import ProjectDetails from './components/ProjectDetails';
+import ProjectsContextProvider from './store/projects-context';
+import MainSection from './components/MainSection';
 
 function App() {
-	const [isCreatingProject, setIsCreatingProject] = useState(false);
-	const [selectedProjectIndex, setSelectedProjectIndex] = useState(null);
-	const [projects, setProjects] = useState([]);
-
-	function saveProject(newProject) {
-		setProjects((prevProjects) => {
-			return [...prevProjects, newProject];
-		});
-		resetProjectSelection();
-	}
-
-	function createNewProject() {
-		setSelectedProjectIndex(null);
-		setIsCreatingProject(true);
-	}
-
-	function resetProjectSelection() {
-		setIsCreatingProject(false);
-		setSelectedProjectIndex(null);
-	}
-
-	function selectProject(projectIndex) {
-		setSelectedProjectIndex(projectIndex);
-	}
-
-	function deleteProject() {
-		setProjects((prevProjects) => {
-			return prevProjects.toSpliced(selectedProjectIndex, 1);
-		});
-		resetProjectSelection();
-	}
-
-	function updateProject(updatedProject) {
-		setProjects((prevProjects) => {
-			const newProjects = prevProjects.map((project, index) => {
-				if (selectedProjectIndex === index) {
-					return updatedProject;
-				} else {
-					return project;
-				}
-			});
-			return newProjects;
-		});
-	}
-
-	let content;
-
-	if (selectedProjectIndex == null && !isCreatingProject) {
-		content = <NoProject createNewProject={createNewProject} />;
-	} else if (isCreatingProject) {
-		content = (
-			<CreateProject
-				saveProject={saveProject}
-				cancel={resetProjectSelection}
-			/>
-		);
-	} else {
-		content = (
-			<ProjectDetails
-				projectDetails={projects[selectedProjectIndex]}
-				updateProject={updateProject}
-				deleteProject={deleteProject}
-			/>
-		);
-	}
-
 	return (
-		<main className="h-screen my-8 flex gap-8">
-			<Sidebar
-				createNewProject={createNewProject}
-				projects={projects}
-				selectProject={selectProject}
-				selectedProject={selectedProjectIndex}
-			/>
-			{content}
-		</main>
+		<ProjectsContextProvider>
+			<main className="h-screen my-8 flex gap-8">
+				<Sidebar />
+				<MainSection />
+			</main>
+		</ProjectsContextProvider>
 	);
 }
 
