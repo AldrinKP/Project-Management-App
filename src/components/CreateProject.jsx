78fa718225ef88ab81a35e48a53eba1a2584fecc
AldrinKP <aldrinpastores@gmail.com>
2024-05-24 12:15:39 +0200

    Handle Modal behavior via state

--- src/components/CreateProject.jsx
+++ src/components/CreateProject.jsx
@@ -1,4 +1,4 @@
-import { useRef, useContext } from 'react';
+import { useRef, useContext, useState } from 'react';
 import { ProjectsContext } from '../store/projects-context';
 import ErrorModal from './ErrorModal';
 import Input from './Input';
@@ -8,9 +8,9 @@ export default function CreateProject() {
 	const title = useRef();
 	const description = useRef();
 	const dueDate = useRef();
-	const invalidValuesDialog = useRef();
 
 	const { saveProject, resetProjectSelection } = useContext(ProjectsContext);
+	const [isOpen, setIsOpen] = useState(false);
 
 	function handleSave() {
 		const newTitle = title.current.value;
@@ -18,7 +18,7 @@ export default function CreateProject() {
 		const newDueDate = dueDate.current.value;
 
 		if (newTitle === '' || newDescription === '' || newDueDate === '') {
-			invalidValuesDialog.current.showModal();
+			setIsOpen(true);
 		} else {
 			const newProject = {
 				title: newTitle,
@@ -29,10 +29,13 @@ export default function CreateProject() {
 			saveProject(newProject);
 		}
 	}
+	function handleInvalidConfirm() {
+		setIsOpen(false);
+	}
 
 	return (
 		<>
-			<ErrorModal ref={invalidValuesDialog} />
+			<ErrorModal open={isOpen} buttonConfirm={handleInvalidConfirm} />
 			<div className="w-[35rem] mt-16 flex-col">
 				<menu className="flex items-center justify-end gap-4 my-4">
 					<li>
--- src/components/ErrorModal.jsx
+++ src/components/ErrorModal.jsx
@@ -1,10 +1,16 @@
-import { forwardRef } from 'react';
+import { useEffect, useRef } from 'react';
 import Button from './Button';
 
-const ErrorModal = forwardRef(function ErrorModal({}, ref) {
+export default function ErrorModal({ open, buttonConfirm }) {
+	const dialog = useRef();
+
+	useEffect(() => {
+		open ? dialog.current.showModal() : dialog.current.close();
+	}, [open]);
+
 	return (
 		<dialog
-			ref={ref}
+			ref={dialog}
 			className="backdrop:bg-stone-900/90 p-4 rounded-md shadow-md"
 		>
 			<h2 className="text-xl font-bold text-stone-500 my-4">
@@ -12,10 +18,8 @@ const ErrorModal = forwardRef(function ErrorModal({}, ref) {
 			</h2>
 			<p className="text-stone-400 mb-4">Please enter valid values</p>
 			<form method="dialog" className="mt-4 text-right">
-				<Button>Close</Button>
+				<Button onClick={buttonConfirm}>Close</Button>
 			</form>
 		</dialog>
 	);
-});
-
-export default ErrorModal;
+}
